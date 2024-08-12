70a990374f60235237c68952ab70b476c1d946d0
Dev-Tams <tammyalemu@gmail.com>
2024-08-12 11:32:02 +0100

    ready for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,14 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
+echo "Caching config/routes..."
+php artisan optimize
 
 echo "Running migrations..."
 php artisan migrate --force
+
